7bf1a73bb242d9669dfc8c0192bf61c344aadc3c
Sumit5151 <sumitshitole5151@gmail.com>
2023-03-17 08:30:58 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Employee
+    {
+
+        //public Employee()  //parameterless or default 
+        //{
+        //    Id = 10;
+        //    Name = "Akshay";
+        //    Salary = 10000;
+        //}
+
+
+        //public Employee()
+        //{
+        //    Id = 0;
+        //    Name = null;
+        //    Salary = 0;
+        //}
+
+
+        public Employee(int a)
+        {
+            Id = 10;
+            Name = "Akshay";
+            Salary = 10000;
+        }
+
+
+
+        public void Putdata()
+        {
+
+        }
+
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+
+        public string GetString()
+        {
+            return "abc";
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }     
+        
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Constructor;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a, b;
+        int num;
+        string str;
+
+
+        Employee emp = new Employee(10);
+
+        emp.GetNumber();
+        emp.GetString();
+
+
+
+
+    }
+}
