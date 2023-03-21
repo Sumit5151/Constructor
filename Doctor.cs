5b3767534fda5bee723a9e211265cf0c4534216b
Sumit5151 <sumitshitole5151@gmail.com>
2023-03-21 08:40:34 +0530

    Static constructor

---
+++ Doctor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public String Degree { get; set; }
+
+        public static int NumberOfDoctors { get; set; }
+
+        static Doctor()
+        {
+            Doctor.NumberOfDoctors = 5;
+        }
+
+
+        public Doctor()           //:this(11,"Amit","BAMS")
+        {
+            this.Id = 10;
+            this.Name = "Munnabai";
+            this.Degree = "MBBS";
+
+        }
+
+
+        public Doctor(int id, string name, string degree) //:this()
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Degree = degree;
+        }
+
+
+        public Doctor(Doctor doctor)
+        {
+            this.Id = doctor.Id;
+            this.Name = doctor.Name;
+            this.Degree = doctor.Degree;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Id =" + this.Id);
+            Console.WriteLine("Name =" + this.Name);
+            Console.WriteLine("Degree =" + this.Degree);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,23 +3,57 @@ using Constructor;
 
 public class Program
 {
+
+    
+
     public static void Main()
     {
+        Doctor doctor2 = new Doctor(11, "Amit", "BAMS");
+        Doctor doctor1 = new Doctor();
 
-        Employee emp1 = new Employee();  //call to parameterless consturctor
-
-        Employee emp2 = new Employee(11,"Vijay",20000);// call to parameterized consturctor
-
-        Employee emp3 = new Employee(12,"Amit",30000);
-
-
-        Employee emp2Copy = new Employee(emp2);// call for copy constructor     
+       
+        Doctor doctor3= new Doctor(12, "Sumit", "MD");
+        Doctor doctor4 = new Doctor(13, "Vijay", "BDS");
 
 
+        Doctor doctor5 = new Doctor(14, "Dinanath", "MBBS");
+   
 
-        emp1.Showdata();
-        emp2.Showdata();
-        emp3.Showdata();
-        emp2Copy.Showdata();
+
+        Doctor newDoctor = new Doctor(doctor5);
+
+
+
+
+        doctor1.ShowData();
+        doctor2.ShowData();
+        doctor3.ShowData();
+        doctor4.ShowData();
+        doctor5.ShowData();
+        newDoctor.ShowData();
+
+
+
+
+
+
+
+
+
+        //Employee emp1 = new Employee();  //call to parameterless consturctor
+
+        //Employee emp2 = new Employee(11,"Vijay",20000);// call to parameterized consturctor
+
+        //Employee emp3 = new Employee(12,"Amit",30000);
+
+
+        //Employee emp2Copy = new Employee(emp2);// call for copy constructor     
+
+
+
+        //emp1.Showdata();
+        //emp2.Showdata();
+        //emp3.Showdata();
+        //emp2Copy.Showdata();
     }
 }
